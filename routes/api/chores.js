3d91b5e74a2ad338a5b29ba05d271157f803ecfe
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-04-15 19:37:07 +0200

    creando componentes dentro de api + create / update / delete / geAll / getById

---
+++ routes/api/chores.js
@@ -0,0 +1,63 @@
+const {
+    getAll,
+    getChoresByUserId,
+    create,
+    updateByIdToken,
+    deleteByIdToken,
+
+} = require('../../models/chores');
+
+const router = require('express').Router();
+const { checkToken } = require('../../middlewares');
+
+// ALL the chores of a user (and we got a JSON) 
+router.get('/:pId', async (req, res) => {
+    // Id of user -> Middleware checkToken!
+
+    try {
+        const chores = await getChoresByUserId(req.params.pId);
+        res.json(chores);
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// Create new chore
+
+router.post('/', checkToken, async (req, res) => {
+    try {
+        req.body.fk_user = req.userId;
+        const result = await create(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+// Delete a Chore
+router.delete('/:idChore', checkToken, async (req, res) => {
+    try {
+        const json = {
+            id: req.params.idChore,
+            fk_usuario: req.userId,
+        };
+        console.log(json);
+        const result = await deleteByIdToken(json);
+        res.json(result);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+// Actualizo un Chore
+router.put('/', async (req, res) => {
+    try {
+        const result = await updateByIdToken(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+
+module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,117 @@
+const {
+    getAll,
+    create,
+    updateById,
+    deleteById,
+    getById,
+    getByEmail,
+} = require('../../models/user');
+
+const { getChoresByUserId } = require('../../models/chores');
+
+
+// PASSWORD AND TOKEN
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const dayjs = require('dayjs');
+const jwt = require('jsonwebtoken');
+const { checkToken } = require('../../middlewares');
+
+/* TOKEN Y MIDDLEWARE */
+
+// Body -> email, password
+router.post('/login', async (req, res) => {
+    // Compruebo si el email está en la BDc
+    const user = await getByEmail(req.body.email);
+    if (user) {
+        // Compruebo si las password coinciden
+        const equal = bcrypt.compareSync(req.body.password, user.password);
+        if (equal) {
+            res.json({
+                success: 'Welcome back!!',
+                token: createToken(user),
+            });
+        } else {
+            res.json({ error: 'Wrong email or password' });
+        }
+    } else {
+        res.json({ error: 'Wrong email or password' });
+    }
+});
+
+function createToken(pUser) {
+    const data = {
+        userId: pUser.id,
+        caduca: dayjs().add(10, 'hours').unix(),
+    };
+
+    return jwt.sign(data, 'to-do-list');
+}
+/* END TOKEN Y MIDDLEWARE */
+
+// Recupera todos los user y devuelve JSON
+router.get('/', async (req, res) => {
+    // Id de user inyectado por el Middleware checkToken!
+    // console.log(req.userId);
+
+    try {
+        const user = await getAll();
+        res.json(user);
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+// Recupera UN unico user by ID para pintarlo por TOKEN
+router.get('/profile', checkToken, async (req, res) => {
+    try {
+        const user = await getById(req.userId);
+        // console.log('holaaaaaaaaaaa', user);
+
+        const chore = await getChoresByUserId(req.userId);
+        user.chores = chore;
+        res.json(user);
+
+    } catch (error) {
+        console.log(error);
+        res.json({ error: error.message });
+    }
+});
+
+// Crear un nuevo user
+// Los datos para crear el user, me llegan a través del BODY
+router.post('/', async (req, res) => {
+    // console.log(req.body);
+    try {
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        const result = await create(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+// Borro un user
+router.delete('/', checkToken, async (req, res) => {
+    try {
+        req.body.id = req.userId;
+        const user = await deleteById(req.body);
+        res.json(user);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+// Actualizo un user
+router.put('/', checkToken, async (req, res) => {
+    try {
+        req.body.id = req.userId;
+        const result = await updateById(req.body);
+        res.json(result);
+    } catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+
+module.exports = router;
